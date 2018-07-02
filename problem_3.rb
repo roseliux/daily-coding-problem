be76741a46f855056894d77123191acde33e5308
roseliux <roxdurazo@gmail.com>
2018-07-02 12:53:20 -0700

    Add problem #3 solution.

--- problem_3.rb
+++ problem_3.rb
@@ -1,6 +1,6 @@
 # This problem was asked by Google.
 
-# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string,
 # and deserialize(s), which deserializes the string back into the tree.
 
 # For example, given the following Node class
@@ -24,54 +24,18 @@ class Node
 	end
 end
 
-def deserialize(string)
-	stack = []
-	string.each_char do |s|
-		if s == '('
-			stack.push(s)
-		elsif  s == ')'
-			stack.pop
-		elsif 'n'
-			tree.val = nil
-		else
-			tree = Node.new(s)
-		end
-	end
-
+def serialize(tree)
+  return Marshal.dump(tree)
 end
 
-def serialize(node=nil)
-	# puts "node #{node.inspect}"
-	return '(n)' if node.nil?
-	"(#{node.val}#{serialize(node.left)}#{serialize(node.right)})"
+def deserialize(serialized)
+  Marshal.load(serialized)
 end
-
-def same_close_tags?(string)
-	stack = []
-	string.each_char do |e|
-		stack.push(e) if e == '('
-		stack.pop if e == ')'
-	end
-	stack.empty?
-end
-
-
 
 require 'minitest/autorun'
 class Tests < Minitest::Test
   def test_example_one
     node = Node.new('root', Node.new('left', Node.new('left.left')), Node.new('right'))
-    assert_equal "(root(left(left.left(n)(n))(n))(right(n)(n)))", serialize(node)
-    # assert deserialize(serialize(node)).left.left.val == 'left.left'
-   end
-
-   def test_example_two
-   	node = Node.new('1', Node.new(2))
-   	assert_equal '(1(2)(n))', serialize(node)
-   end
-
-   def test_example_three
-     assert same_close_tags?('(root(left(left.left(n)(n))(n))(right(n)(n)))')
-     assert !same_close_tags?('(((())')
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
    end
 end
